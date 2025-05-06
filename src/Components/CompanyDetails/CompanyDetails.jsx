057a0546c11e794e2602bdf99848ae0ffc283590
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-05-06 23:23:42 +0600

    company details page done

--- src/Components/CompanyDetails/CompanyDetails.jsx
+++ src/Components/CompanyDetails/CompanyDetails.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import CompanyDetailsCard from '../CompanyDetailsCard/CompanyDetailsCard';
+
 
 const CompanyDetails = () => {
     const obj = useParams();
@@ -11,10 +15,18 @@ const CompanyDetails = () => {
         const companyDetails = companiesData.find( company => company.id == id);
         setCompany(companyDetails);
     }, [companiesData,id])
-    // console.log(company)
+    //  console.log(company)
     return (
         <div>
-            company
+            <header className='bg-base-200 '>
+                <Navbar/>
+            </header>
+            <main>
+                <CompanyDetailsCard company={company}></CompanyDetailsCard>
+            </main>
+            <footer className='bg-red-50'>
+                <Footer></Footer>
+            </footer>
         </div>
     );
 };
---
+++ src/Components/CompanyDetailsCard/CompanyDetailsCard.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const CompanyDetailsCard = ({company}) => {
+    const {name,bannerImage,website,headOfficeAddress} = company;
+    // const {title,jobType,salary, description, requirements,website} = company.jobs
+    const manyJobs = company.jobs;
+    // console.log(jobs)
+   
+    return (
+        <div className='bg-gray-100'>
+            <div className='w-11/12 mx-auto p-5'>
+                <div className='flex flex-col md:flex-row gap-5 items-center justify-center mb-5'>
+                    <div className='w-[60%] md:w-[30%]'>
+                        <img className='w-100 md:w-60' src={bannerImage} alt="" />
+                    </div>
+                    <div className='space-y-2 text-center md:text-start'>
+                        <h2 className='font-bold text-3xl md:text-5xl text-shadow-slate-500'>{name}</h2>
+                        <p className='text-sm md:text-lg'>Address: {headOfficeAddress}</p>
+                        <a href="" className='text-green-600'>{website}</a>
+                    </div>
+                    
+                </div>
+                <hr className='w-full bg-slate-50'/>
+                <div className='grid grid-cols-1 sm:grid-cols-2 gap-5'>
+                   {
+                            //jobs prothome undefined thake tai tokhon map hbe na. tai ? diye optional chaining korlam
+                            manyJobs?.map(job => 
+                            <div key={job.id} className='space-y-2'>
+                                    <h3 className='font-bold text-red-950 text-xl'>Job Title: {job.title}</h3>
+                                    <p className='text-base'>Salary: {job.salary}</p>
+                                    <p>job Type: {job.jobType}</p>
+                                
+                                        {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+                                        <button className="btn btn-outline btn-info" onClick={()=>document.getElementById('my_modal_1').showModal()}>Details</button>
+                                        <dialog id="my_modal_1" className="modal">
+                                        <div className="modal-box space-y-1">
+                                            <h3 className="font-bold text-lg text-red-800">Job Description: <span className='text-black'>{job.description}</span></h3>
+                                            <p className="py-4 text-red-600 text-sm">Requirements: <span className='text-black'>{job.requirements}</span></p>
+
+                                            <p className="py-4 text-red-600 text-sm">Location: <span className='text-black'>{job.location}</span></p>
+                                            <div className="">
+                                            <form method="dialog" className='flex justify-between'>
+                                                {/* if there is a button in form, it will close the modal */}
+                                                <a href={website} className='btn btn-success'>Apply</a>
+                                                <button className="btn btn-">Close</button>
+                                            </form>
+                                            </div>
+                                        </div>
+                                        </dialog>
+                                    
+                                    <hr className='w-full bg-slate-50'/>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CompanyDetailsCard;
